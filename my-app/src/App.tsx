d2136f59e1868468d3cfdfdedc13c1071a0f5553
oskarmay <6lolek9@gmail.com>
2021-04-19 19:50:46 +0200

    better notes from class

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,13 +5,13 @@ import Counter from './components/Counter';
 
 const App = () => {
   const addedNumber=addNumber(1,2);
-  const [changedNumber, setChangedNumber] = useState(0);
+  const [changedNumber, setChangedNumber] = React.useState(0);
   const handleNumberChange = (newNumber: number) => {
-    console.log(newNumber)
+    setChangedNumber(newNumber);
   }
   const liczbaMniejszaodZera = (liczba: number) => {
     if(liczba < 0) {
-      return (<div>Liczba jest mniejsza od 0</div>)
+      return (<div>Liczba jest mniejsza od 0</div>);
     }
   } 
   return (
@@ -21,6 +21,7 @@ const App = () => {
         {
           changedNumber > 0 && (<div> Liczba jest większa od 0</div>) 
         }
+        {liczbaMniejszaodZera(changedNumber)}
       </CustomHeader>  
     </div>
   );
